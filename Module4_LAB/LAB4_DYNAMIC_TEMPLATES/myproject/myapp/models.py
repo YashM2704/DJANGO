2732c3fde613daaa12e215ed24f006a8a9f55b50
YashM2704 <ypmahajan27@gmail.com>
2025-03-06 01:34:59 +0530

    MODIFIED 2.0

--- Module4_LAB/LAB4_DYNAMIC_TEMPLATES/myproject/myapp/models.py
+++ Module4_LAB/LAB4_DYNAMIC_TEMPLATES/myproject/myapp/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
+# Create your models here.
 class Menu(models.Model):
-    name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    name = models.CharField(max_length=200)
+    price = models.IntegerField()
 
     def __str__(self):
-        return f"{self.name} - ${self.price}"
+        return self.name
